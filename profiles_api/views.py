376574c0faadd5768bb0b272a62b8833da857cc1
SantiDani <profsantiRP@gmail.com>
2021-06-03 23:25:34 +0200

    add Calls HTTP
    Add get, put, patch, delete

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,16 @@
 from rest_framework.views import  APIView
 from rest_framework.response import Response
 
+#import from serializer
+from rest_framework import status
+from profiles_api import  serializer
+
 # Create your views here.
 
 class HelloAPIView(APIView):
     """ API View of Hello world"""
+
+    serializer_class = serializer.HelloSerializers
 
     def get(self, request, format=None):
         """ Return list of characteristics """
@@ -18,3 +24,33 @@ class HelloAPIView(APIView):
 
         # Need a list o dictionary for transform to JSON
         return Response({'message': 'Hello', 'an_appview': an_apiview})
+
+    def post(self, request):
+        """ Create a message with our name"""
+        serializer = self.serializer_class(data= request.data)
+
+        if serializer.is_valid():
+            # 'name' is the attribute defined in profiles_api/serializer.py
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return  Response({'message': message})
+        else:
+            #
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """ Update a object
+            In this case we dont use ID (PK)
+        """
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
+
